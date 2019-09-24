8d6e0be0233a4d0b7c006d8793f4b1f1296a915e
df.nino10 <df.nino10@uniandes.edu.co>
2019-09-23 22:27:48 -0500

    Se inicia con un ejemplo, se crean bases de datos y se conecta

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb+srv://dfnino10:DB_Password123@cluster0-xlopf.mongodb.net/test?retryWrites=true&w=majority';
+
+const client = new MongoClient(url);
+
+const readTweets = (resolve, reject) => {
+  client.connect((err) => {
+    if (err){
+      reject(err);
+      throw err;
+    }
+    let db = client.db('DavidDb');
+    let colTweets = db.collection('Tweets');
+    colTweets.find({}).toArray((err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+};
+
+
+/* GET tweets. */
+router.get('/tweets', function(req, res) {
+  readTweets(
+    (data) => res.json(data),
+    (err) => res.send(err));
+});
+
+module.exports = router;
